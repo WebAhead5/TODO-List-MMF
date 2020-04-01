f1601d6d28f8a1cfb26f289c675eeb88d9a28192
Marwanrizik91 <marwan.rizik@gmail.com>
2020-04-01 11:27:28 +0300

    tests for addTodo

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,33 @@
 var test = require('tape');
 var logic = require('../scripts/logic');
 
-test('Example test', function(t) {
-  t.pass();
+
+test('add element for an empty array', function(t) {
+  var todos = []
+  var element = { description: 'make tea'}
+  var expected = [
+    { description: 'make tea', "Done": false}
+  ]
+  var actual = logic.addTodo(todos, element)
+  t.deepEqual(actual, expected, 'should be equal');
   t.end();
 });
+
+test('add element for not an empty array', function(t) {
+  var todos = [{ description: 'make tea' }];
+  var makeEggs = { description: 'make eggs' };
+  var expected = [
+    { description: 'make tea' },
+    { description: 'make eggs', "Done": false }
+  ]
+  var actual = logic.addTodo(todos, makeEggs)
+  t.deepEqual(actual, expected, 'should be equal');
+  t.end();
+});
+
+
+
+
+
+
+
