880abde891214849f7e8530720c3888cabf1faef
Marwanrizik91 <marwan.rizik@gmail.com>
2020-04-02 10:35:45 +0300

    fixed paths again

--- scripts/dom.js
+++ scripts/dom.js
@@ -8,47 +8,54 @@
 
 (function() {
     // This is the dom node where we will keep our todo
+
     var container = document.getElementById('todo-container');
     var addTodoForm = document.getElementById('add-todo');
   
     // This function takes a todo, it returns the DOM node representing that todo
-    var createTodoNode = function(todo) {
-      var todoNode = document.createElement('li');
-      console.log(todoNode)
-      var markTodobtn = document.createElement('img')
-      markTodobtn.src = './res/empty.png'
-      // this changes the ui of the checkmarks and row colors when done.
-      todoNode.setAttribute("id", todo.id)
-      if(todo.done){
-        todoNode.style.background = "red";
-        markTodobtn.src = './res/checkbox.png';
 
+    var createTodoNode = function(todo) {
+    var todoNode = document.createElement('li');
+    var markTodobtn = document.createElement('img');
+
+    // init uncheck box
+    markTodobtn.src = '../res/empty.png';
+
+    // this changes the ui of the checkmarks and row colors when done
+    todoNode.setAttribute("id", todo.id)
+    if(todo.done){
+      //if the box is clicked change to a checked box and change the background color.
+      todoNode.style.background = "red";
+      markTodobtn.src = '../res/checkbox.png';
       } else {
         todoNode.style.background = "";
       }
 
       // this adds the delete button
       var deleteButtonNode = document.createElement('img');
-      deleteButtonNode.src = './res/trash.png'
+      deleteButtonNode.src = '../res/trash.png'
       deleteButtonNode.setAttribute("class", "deletebtn")
       deleteButtonNode.addEventListener('click', function(event) {
-        var newState = todoFunctions.deleteTodo(state, event.path[1].id);
-        update(newState);
+
+       var newState = todoFunctions.deleteTodo(state, event.path[1].id);
+       update(newState);
+
       });
+
        todoNode.appendChild(deleteButtonNode);
        todoNode.appendChild(document.createTextNode(todo.description));
       // adds markTodo button 
       
-        markTodobtn.setAttribute("class", "markbtn")
+      markTodobtn.setAttribute("class", "markbtn")
 
-        markTodobtn.addEventListener('click', function(event) {
-        var newState = todoFunctions.markTodo(state, event.path[1].id);
-        update(newState)
+      markTodobtn.addEventListener('click', function(event) {
+
+       var newState = todoFunctions.markTodo(state, event.path[1].id);
+       update(newState)
+
       });
       todoNode.appendChild(markTodobtn);
       
-      // add classes for css
-
       // reset input after submission TODO
       
   
