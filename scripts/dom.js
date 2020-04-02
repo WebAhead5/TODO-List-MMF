971327db16f8a7e5316adc891b265e6a61c16da7
Marwanrizik91 <marwan.rizik@gmail.com>
2020-04-02 11:31:47 +0300

    added sortFunction and button into the ui

--- scripts/dom.js
+++ scripts/dom.js
@@ -6,6 +6,8 @@
       { id: -1, description: 'Delete me and start now!', done: false },
  ];
 
+ var sortAsec = false;
+
 (function() {
     // This is the dom node where we will keep our todo
 
@@ -26,6 +28,7 @@
     if(todo.done){
       //if the box is clicked change to a checked box and change the background color.
       todoNode.style.background = "red";
+      todoNode.style.textDecoration = "lineThrough"
       markTodobtn.src = '../res/checkbox.png';
       } else {
         todoNode.style.background = "";
@@ -55,18 +58,34 @@
 
       });
       todoNode.appendChild(markTodobtn);
+
+      // adds sort button
+      
       
       // reset input after submission TODO
       
   
       return todoNode;
     };
-  
+    
+    var sortBtn = document.querySelector('.sortBy')
+      sortBtn.addEventListener('click', function(event) {
+        var newState;
+        if(sortAsec) {
+          newState = todoFunctions.sortTodos(state, compareFunction("id"));
+        } else {
+          newState = todoFunctions.sortTodos(state, compareFunction("id", "desc"));
+        }
+        sortAsec = !sortAsec;
+        update(newState)
+      })
+
     // bind create todo form
     if (addTodoForm) {
         addTodoForm.addEventListener('submit', function(event) {
             event.preventDefault();
             var description = event.target.description.value;
+            addTodoForm.reset();
             var obj = Object.assign({},{description: description});
             var newState = todoFunctions.addTodo(state,obj);
             if(Array.isArray(newState)){
